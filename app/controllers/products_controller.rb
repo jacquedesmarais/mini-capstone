c6e2e75dcb8eaf6119436605b57efc17f0176601
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-08-29 20:51:40 -0500

    added files, connected, page, created single and all product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,15 @@
 class ProductsController < ApplicationController
+
+  def one_product_action
+    @title = "Products"
+    @product = Product.first
+    render 'one_product_page.html.erb'
+  end
+
+  def all_product_action
+    @title = "All Products"
+    @products = Product.all
+    render 'all_product_page.html.erb'
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(
+                      name: "Seahorse",
+                      price: 30,
+                      image: "http://www.ccmnh.org/Images/cms/seahorse_4_CALENDAR.png?format=jpg&mode=max&scale=both&width=410",
+                      description: "orange, small, cute"
+                      )
+
+product.save
+
+product = Product.new(
+                      name: "Whale Shark",
+                      price: 90000000,
+                      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Whale_shark_Georgia_aquarium.jpg/1200px-Whale_shark_Georgia_aquarium.jpg",
+                      description: "giant, calm, bottom-feeder"
+                      )
+
+product.save
+
+product = Product.new(
+                      name: "Octopus",
+                      price: 4000,
+                      image: "https://i.ytimg.com/vi/epbJi35lzEs/maxresdefault.jpg",
+                      description: "sucky, scary, very bottom of the ocean"
+                      )
+
+product.save
+
+product = Product.new(
+                      name: "Blue Tang",
+                      price: 700,
+                      image: "http://3.bp.blogspot.com/-Z25Hb5Hp1eY/TsJFyhcn4XI/AAAAAAAACLg/NKgzeU75T3o/s1600/Blue+Tang5.jpg",
+                      description: "blue, flat, fast"
+                      )
+
+product.save
+
+product = Product.new(
+                      name: "Beluga Whale",
+                      price: 7000000,
+                      image: "http://www.georgiaaquarium.org/images/default-source/default-album/gaq-galleries-485ac23c413cabb6b4c8326ff00008b2be6.jpg?sfvrsn=1",
+                      description: "playful, giant puppies, tricks"
+                      )
+
+product.save
+
+product = Product.new(
+                      name: "Goldfish",
+                      price: 1,
+                      image: "https://i.pinimg.com/736x/c3/96/d4/c396d4c57495616ca9bfa0a932b14bb1--pet-fish-fish-fish.jpg",
+                      description: "gold, cheap, multicolored"
+                      )
+
+product.save
