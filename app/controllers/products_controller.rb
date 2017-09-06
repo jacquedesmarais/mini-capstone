d94c982f2701b88e752c48df3cac4ce3769585ab
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-09-05 21:25:06 -0500

    mini capstone edit, update, and destroy routes as well as added flash

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,9 +5,52 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    
+  end
+
+  def create
+    product = Product.new(
+                          name: params[:name],
+                          price: params[:price],
+                          image: params[:image],
+                          description: params[:description]
+                          )
+
+    product.save
+    flash[:success] = "You added a #{ product.name } to the tank"
+    redirect_to "/products/#{ product.id }" 
+  end
+
   def show
     @title = "Product"
     @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+
+    product.assign_attributes(
+                              name: params[:name],
+                              price: params[:price],
+                              image: params[:image],
+                              description: params[:description]
+                              )
+
+    product.save
+    flash[:success] = "You edited your #{ product.name }. Be sure to say hi next time you visit."
+    redirect_to "/products/#{ product.id }"
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    flash[:warning] = "You successfully killed a #{ product.name }"
+    redirect_to "/products"
   end
 
 end
